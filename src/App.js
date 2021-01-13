a736470c334d397abd41eecaf770a1627a762e99
bxw0405 <west.barry@att.net>
2021-01-13 03:37:35 -0600

    Change app.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Prepare for Disclosure.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Alien Technology
       </header>
     </div>
   );
